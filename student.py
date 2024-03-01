321fb2472bd07b8f3ee088f0aac8f55d244ca17b
JosePavas <jpavas887@soyudemedellin.edu.co>
2024-02-29 19:15:09 -0500

    commit 2

--- student.py
+++ student.py
@@ -4,16 +4,34 @@ class Student:
         self.age = age 
         self.grades:list = grades
     
-    def add_grade(self, suma_grades, promedio, contar_grades):
-        self.suma_grades = suma_grades
-        self.promedio = promedio
-        self.contar_grades = contar_grades
-        suma_grades = 0
-        for nota in self.grades:
-            suma_grades += nota
-        contar_grades = self.grades.__len__
-        promedio = suma_grades / contar_grades
+    def add_grade(self, nota):
+         self.grades.append(nota)
+        
     
-    def avarge_grades(seld):
-        pass        
+    def avarage_grade(self):
+        
+        suma_grades = sum(self.grades)
+        contar_grades = len(self.grades)
+        promedio = suma_grades / contar_grades
+        return promedio
+lista = [
+    {
+        "name":"jose",
+        "age":20, 
+        "grades":[1,2,3,4,5]
+    },
+    {
+       "name":"andres",
+       "age":23,
+       "grades":[3,4,5,1,1] 
+        },
+    {
+        "name":"alejandra",
+        "age":21,
+        "grades":[4,4,5,3,2]
+    }]
+
+    
+    
+   
                
